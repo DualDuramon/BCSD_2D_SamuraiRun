36169b248fd646b3b16adf1432ac875d5ea9f566
DualDura <jdsan216@gmail.com>
2024-05-18 15:56:46 +0900

    Make_PlayerJump

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,29 +6,38 @@ public class PlayerScript : MonoBehaviour
 {
 
     //점프 관련
-    bool isJump = false;                //플레이어 jump 상태 변수
-    bool isTop = false;                 //플레이어가 최대 높이에 있나 체크 변수
+    private bool isJump = false;                //플레이어 jump 상태 변수
+    private bool isTop = false;                 //플레이어가 최대 높이에 있나 체크 변수
+    private Vector2 startPosition;              //플레이어 처음 위치
     public float jumpHeight = 0.0f;     //플레이어 jump 높이
     public float jumpSpeed = 0.0f;      //플레이어 jump 속도
 
-    Vector2 startPosition;
+    //점프 애니메이션
+    Animator animator;      //플레이어 애니메이터
 
     void Start()
     {
         startPosition = transform.position;     //jump관련 : 현재 포지션 저장 
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
+        
         //점프 관련
         if (Input.GetButtonDown("Jump"))
         {
             isJump = true;
+            animator.SetBool("isJump", true);
 
         }
         else if (transform.position.y <= startPosition.y) {
             isJump = false;
+            animator.SetBool("isJump", false);
+
             isTop = false;
+            animator.SetBool("isTop", false);
+
             transform.position = startPosition;
         }
 
@@ -42,6 +51,7 @@ public class PlayerScript : MonoBehaviour
             else
             {
                 isTop = true;
+                animator.SetBool("isTop", true);
             }
         }
 
